9576e1373ae3a11c06b18575be3f4e94e9d83970
junghyun397@gmail.com <junghyun397@gmail.com>
2020-02-25 04:05:12 +0900

    add shell-script for easy-build

---
+++ build_android.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+if [ -n "$2" ]; then
+  export BUILD_DIR_PATH=$2
+else
+  export BUILD_DIR_PATH="$HOME/projects/virtual-flight-throttle/build"
+fi
+
+if [ -n "$3" ]; then
+  export BUNDLETOOL_PATH=$3
+else
+  export BUNDLETOOL_PATH="$HOME/Android/bundletool-all-0.12.0.jar"
+fi
+
+echo "[i] start build appbundle..."
+
+~/flutter/bin/flutter build appbundle
+cp ./build/app/outputs/bundle/release/app-release.aab "$BUILD_DIR_PATH"/appbundle/vft-flight-throttle-"$1".aab
+
+echo "[o] succeed build appbundle."
+
+echo "[i]start build mono-apks..."
+
+java -jar "$BUNDLETOOL_PATH" build-apks --bundle=./build/app/outputs/bundle/release/app-release.aab --output="$BUILD_DIR_PATH"/apk/vft-flight-throttle-"$1".apks --mode=universal
+unzip "$BUILD_DIR_PATH"/apk/vft-flight-throttle-"$1".apks -d "$BUILD_DIR_PATH"/apk/
+
+echo "[o] succeed build mono-apks."
+
+rm "$BUILD_DIR_PATH"/apk/toc.pb
+rm "$BUILD_DIR_PATH"/apk/vft-flight-throttle-"$1".apks
+
+mv "$BUILD_DIR_PATH"/apk/universal.apk "$BUILD_DIR_PATH"/apk/vft-flight-throttle-"$1".apk
+
+echo "[o] succeed build appbundle and apk with version $1"
